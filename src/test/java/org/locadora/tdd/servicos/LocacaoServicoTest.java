41b4a020e73055ebc7847c27c45ee078da998283
Collins Daniel <cdpm.sistemas@gmail.com>
2018-12-06 20:22:09 -0200

    ponto do rogério

--- src/test/java/org/locadora/tdd/servicos/LocacaoServicoTest.java
+++ src/test/java/org/locadora/tdd/servicos/LocacaoServicoTest.java
@@ -3,39 +3,36 @@ package org.locadora.tdd.servicos;
 import org.junit.Test;
 
 public class LocacaoServicoTest {
-    
-        @Test
-        public void deveAlugarFilme() {
-            //cenario
-            
-            
-            //acao
-         
-            
-            //verificacao
-            
-            
-        }
-	
+
+	@Test
+	public void deveAlugarFilme() {
+		// cenario
+		//.
+		// acao
+
+		// verificacao
+
+	}
+
 	@Test
 	public void naoDeveAlugarFilmeSemEstoque() {
 	}
-	
+
 	@Test
 	public void naoDeveAlugarFilmeSemUsuario() {
-            
+
 	}
 
 	@Test
 	public void naoDeveAlugarFilmeSemFilme() {
 	}
-	
+
 	@Test
-	public void deveEnviarEmailParaLocacoesAtrasadas(){
+	public void deveEnviarEmailParaLocacoesAtrasadas() {
 	}
-	
+
 	@Test
-	public void deveProrrogarUmaLocacao(){
+	public void deveProrrogarUmaLocacao() {
 	}
 
 }
